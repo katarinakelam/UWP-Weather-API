1fedba0000af851f88ddd71e1a7b3e8dbb8ea6e7
katarinakelam <kkelam00@fesb.hr>
2017-05-22 16:01:04 +0200

    first commit; project still in progress

--- Weather/MainPage.xaml.cs
+++ Weather/MainPage.xaml.cs
@@ -26,5 +26,10 @@ namespace Weather
         {
             this.InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
